495777ce361da676a11ddabd58e24ff898b4a151
Mauricio Suarez <kaiserwolf18@gmail.com>
2024-09-30 17:51:42 -0600

    Actualización de metodo processRequest
    Se agregaron los casos para procesar la solicitud de impresión del cliente a la impresora

--- Practica04/src/RequestPrint.java
+++ Practica04/src/RequestPrint.java
@@ -10,10 +10,23 @@ public class RequestPrint {
 	}
 
 	public void processRequest(String typePrint) {
+		Cliente c = new Cliente();
 		if (typePrint == null) {
 			return null
 		} else if (typePrint.equalsIgnoreCase("color")) {
-			
+			if (c.getArea().equalsIgnoreCase("mercadotecnia")) {
+				System.out.println("El cliente es de mercadotecnica, imprimira a color");
+			} else if (!c.getArea().equalsIgnoreCase("mercadotecnia")) {
+				System.out.println("El cliente no es de mercadotecnica, no se puede imprimir a color");
+			}
+		} else if (typePrint.equalsIgnoreCase("Blanco y negro")) {
+			if (!c.getArea().equalsIgnoreCase("mercadotecnia")) {
+				System.out.println("El cliente va a imprimir a color");
+			} else if (c.getArea().equalsIgnoreCase("mercadotecnia")) {
+				System.out.println("El cliente es de mercadotecnica, no se puede imprimir a blanco y negro");
+			}
+		} else {
+			System.out.println("No se puede proceder la solicitud de impresion");
 		}
 	}
 
