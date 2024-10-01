3be84018ad57968464fd34b2ff88ea83df88c398
Mauricio Suarez <kaiserwolf18@gmail.com>
2024-10-01 02:33:07 -0600

    Correccion del metodo processRequest, se ocupa el cliente para hacer los demas

--- Practica04/src/RequestPrint.java
+++ Practica04/src/RequestPrint.java
@@ -14,7 +14,25 @@ public class RequestPrint {
 	 **/ 
 	public void processRequest() {
 		cliente.requestPrinting();
-		System.out.println("El cliente:" + cliente.getName() + " " + "solicito una impresion" + typePrint + " " + "el cliente es del piso: " cliente.getPiso() + "Area de Trabajo: " + cliente.getArea());
+		if (typePrint == null) {
+			System.out.println("No ingreso el tipo de impresion que quiere el cliente");
+		} else if (typePrint.equalsIgnoreCase("color")) {
+			if (c.getArea().equalsIgnoreCase("mercadotecnia")) {
+				System.out.println("El cliente es de mercadotecnica, imprimira a color");
+			} else if (!c.getArea().equalsIgnoreCase("mercadotecnia")) {
+				System.out.println("El cliente no es de mercadotecnica, no se puede imprimir a color");
+			} else {
+				System.out.println("No perteneces al area de trabajo correspondiente para hacer impresion a color");
+			}
+		} else if (typePrint.equalsIgnoreCase("Blanco y negro")) {
+			if (!c.getArea().equalsIgnoreCase("mercadotecnia")) {
+				System.out.println("El cliente va a imprimir a color");
+			} else if (c.getArea().equalsIgnoreCase("mercadotecnia")) {
+				System.out.println("El cliente es de mercadotecnica, no se puede imprimir a blanco y negro");
+			}
+		} else {
+			System.out.println("No se puede proceder la solicitud de impresion");
+		}
 	}
 
 
