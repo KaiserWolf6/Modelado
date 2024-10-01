ef94853cbe3bc02922a633517a4b944575b307f3
Mauricio Suarez <kaiserwolf18@gmail.com>
2024-10-01 02:41:46 -0600

    Se actualizaron los metodos printBW & printC, donde se usara el request y el metodo processRequest para ver si se puede hacer la impresion a blanco y negro o a color

--- Practica04/src/Print.java
+++ Practica04/src/Print.java
@@ -9,11 +9,21 @@ public class Print  {
 		return instance;
 	}
 
+	/**
+	 * Imprime a blanco y negro el documento
+	 **/ 
 	public void printBW() {
+		RequestPrint rp = new RequestPrint();
+		rp.processRequest();
 		System.out.println("Esta imprimiendo a blanco y negro...");
 	}
 
+	/**
+	 * Imprime a color el documento
+	 **/ 
 	public void printC() {
+		RequestPrint rp = new RequestPrint();
+		rp.processRequest();
 		System.out.println("Esta imprimiendo a color...");
 	}
 
