29cd4256dd89bd49faf3ba547b62c5119cb2d16a
Mauricio Suarez <kaiserwolf18@gmail.com>
2024-09-30 21:29:29 -0600

    Se actualizo el metodo de allowColorPrints
    Se hizo modificacion al metodo sobrecargado allowColorPrints
    El detalle esta, en que no supe como meter el boolean, ya que en la clase RequestPrint tiene algo muy similar y pues simplemente cree una solicitud nueva ahi para llamar al metodo

--- Practica04/src/ProxyPrinter.java
+++ Practica04/src/ProxyPrinter.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 
 public class ProxyPrinter implements Serializable, Permissions{
 
-	private Print printer;
+	private Print printer = Print.getInstance();
 	private boolean verifyPermissions;
 
 	public void print() {
@@ -18,9 +18,21 @@ public class ProxyPrinter implements Serializable, Permissions{
 		
 	}
 
+	/**
+	 * Permite que se realice la impresion de color en la impresora de acuerdo a la solicitud
+	 * que se tenga.
+	 * Verificaremos si el cliente corresponde al area de trabajo que tiene acceso a la impresion de color
+	 **/ 	
 	@Override
 	public void allowColorPrints() {
-		
+		verifyPermissions = false;
+		RequestPrint request = new RequestPrint();
+		if (request == null) {
+			System.out.println("No hay solicitud de impresion a color");
+		} else {
+			request.processRequest();
+			verifyPermissions = true;
+		}
 	}
 
 }
