6193b8c416f3426e1eaf36b22482ee9538aecc9c
Mauricio Suarez <kaiserwolf18@gmail.com>
2024-10-02 00:11:40 -0600

    Se documento el codigo en javadoc de la clase client

--- Practica04/src/Client.java
+++ Practica04/src/Client.java
@@ -6,13 +6,22 @@
  */
 public class Client {
 
-	// Atributos
+	/*Atributo del nombre del cliente*/	
 	private String name;
+	/*Atributo del rfc del cliente*/	
 	private String rfc;
+	/*Atributo del area del cliente*/	
 	private String area;
+	/*Atributo del piso donde trabaja del cliente*/	
 	private int piso;
 
-	// Constructor
+	/**
+	 * Constructor de la clase Cliente
+	 * @param name es el nombre del cliente
+	 * @param rfc es el rfc del cliente
+	 * @param area es el area donde trabaja el cliente
+	 * @param piso es el numero de piso donde se localiza el cliente
+	 **/ 
 	public Client(String name, String rfc, String area, int piso) {
 		this.name = name;
 		this.rfc = rfc;
@@ -20,40 +29,74 @@ public class Client {
 		this.piso = piso;
 	}
 
-	// Setters
+	/**
+	 * Sobreescribe o escribe el nombre del cliente
+	 * @param name es el nombre que se le va asignar al cliente
+	 **/ 
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Sobreescribe o escribe el area del trabajo del cliente
+	 * @param area es el area que se le va asignar al cliente
+	 **/ 	
 	public void setArea(String area) {
 		this.area = area;
 	}
 
+	/**
+	 * Sobreescribe o escribe el rfc del cliente
+	 * @param rfc es el rfc que se va a poner al cliente
+	 **/	
 	public void setRFC(String rfc) {
 		this.rfc = rfc;
 	}
 
+	/**
+	 * Sobreescribe o escribe el piso donde trabaja del cliente
+	 * @param piso es el piso que se le va asignar al cliente
+	 **/ 	
 	public void setPiso(int piso) {
 		this.piso = piso;
 	}
 
-	// Getters
+	/**
+	 * Regresa el nombre del cliente
+	 * @return regresa una cadena que es el rfc del cliente
+	 **/ 
 	public String getRFC() {
 		return rfc;
 	}
 
+	/**
+	 * Regresa el nombre del cliente
+	 * @return regresa una cadena que es el nombre del cliente
+	 **/ 	
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Regresa el nombre del cliente
+	 * @return regresa una cadena que es el area de trabajo del cliente
+	 **/ 	
 	public String getArea() {
 		return area;
 	}
 
+	/**
+	 * Regresa el nombre del cliente
+	 * @return regresa una cadena que es el piso del cliente
+	 **/ 	
 	public int getPiso() {
 		return piso;
 	}
 
+	/**
+	 * Manda una solicitud de impresion el cliente a la impresora
+	 * @param request es la solicitud de impresion que manda el cliente a la impresora
+	 **/ 	
 	public void requestPrinting(String request) {
 		System.out.println("El empleado " + name + " " + " del area: " + area + " quiere imprimir");
 	}
