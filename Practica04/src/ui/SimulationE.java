97829aebd5ab35317a67716b230f53956e9de0b6
Mauricio Suarez <kaiserwolf18@gmail.com>
2024-10-02 23:54:01 -0600

    Correccion del simulation E

--- Practica04/src/ui/SimulationE.java
+++ Practica04/src/ui/SimulationE.java
@@ -14,63 +14,64 @@ import java.net.UnknownHostException;
  */
 public class SimulationE {
 
-	public static void main(String[] args) {
-		if ("server".equals(args[0])) {
-			startServer();
-		} else if ("client".equals(args[0])) {
-			startClient();
-		}
-	}
+    public static void main(String[] args) {
+        if ("server".equals(args[0])) {
+            startServer();
+        } else if("client".equals(args[0])){
+            startClient();
+        }       
+    }
 
-	/**
-	 * Inicializa el server para administrar las solicitudes
-	 */
-	public static void startServer() {
-		System.out.println("Iniciando el servidor...");
-		String archivo = "Soy un archivo";
-		try {
-			ServerSocket server = new ServerSocket(8080);
-			while (true) {
-				Socket s = server.accept();
-				Server rmp = new Server(s);
-				ProxyPrinter printer = (ProxyPrinter) rmp.receiveRequest();
-				System.out.println("Verificando el tipo de impresion");
-				printer.allowColorPrints();
-				System.out.print("Impresion verificada");
-				rmp.send(archivo);
-				System.out.println("Se ha enviado");
-				rmp.close();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    /**
+     * Inicializa el server para administrar las solicitudes
+     */
+    public static void startServer() {
+    	System.out.println("Iniciando el servidor...");
+    	String archivo = "Soy un archivo";
+    	try {
+    		ServerSocket server = new ServerSocket(8080);
+    		while (true) {
+    			Socket s = server.accept();
+    			Server rmp = new Server(s);
+    			ProxyPrinter printer = (ProxyPrinter)rmp.receiveRequest();
+    			System.out.println("Verificando el tipo de impresion");
+    			printer.allowColorPrints();
+    			System.out.print("Impresion verificada");
+                rmp.send(archivo);
+                System.out.println("Se ha enviado");		
+    			rmp.close();
+    		}
+    	} catch(IOException e) {
+    		e.printStackTrace();
+    	}
 
-	}
+    }
 
-	/**
-	 * Da inicio a la solicitud de impresion al cliente
-	 */
-	public static void startClient() {
-		System.out.println("Cliente que solicita la impresion");
-		String archivo = "Archivo a imprimir";
-		String archivo2 = "Archivo a imprimir a color";
-		try {
-			Socket s = new Socket("localhost", 8080);
-			Server rmp = new Server(s);
-			Client c = new Client("Samuel", "SBKBKDGDK0886", "Mercadotecnia", 4);
-			Client r = new Client("Rodrigo", "RDHBVHNON07656", "Desarrollo", 4);
-			c.requestPrinting(archivo2);
-			c.requestPrinting(archivo);
-			System.out.println("Los empleados solicitaron una impresion");
-			ProxyPrinter printer = new ProxyPrinter();
-			System.out.println("Enviando la solicitud al servidor");
-			printer.print();
-			rmp.close();
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * Da inicio a la solicitud de impresion al cliente
+     */
+    public static void startClient() {
+    	System.out.println("Cliente que solicita la impresion");
+    	String archivo = "Archivo a imprimir";
+    	String archivo2 = "Archivo a imprimir a color";
+    	try {
+    		Socket s = new Socket("localhost", 8080);
+    		Server rmp = new Server(s);
+            //Print printer = Print.getInstance();
+    		Client c = new Client("Samuel", "SBKBKDGDK0886","Mercadotecnia", 4);
+    		Client r = new Client("Rodrigo", "RDHBVHNON07656", "Desarrollo", 4);
+    		c.requestPrinting(archivo2);
+    		c.requestPrinting(archivo);
+    		System.out.println("Los empleados solicitaron una impresion");
+    		ProxyPrinter p = new ProxyPrinter();
+    		System.out.println("Enviando la solicitud al servidor");
+    		p.print();
+    		rmp.close();
+    	} catch(UnknownHostException e){
+    		e.printStackTrace();
+    	} catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    }
 
 }
